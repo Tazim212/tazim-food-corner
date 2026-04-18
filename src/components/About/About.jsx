f3b605a887d4a4f2a64b574d05fa99a6606f50a1
Tazim212 <iftekhartazim17@gmail.com>
2026-04-18 21:59:42 +0600

    about section updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className='bg-cyan-900 pb-8'>
             <h1 className='text-3xl text-center font-bold py-3'>About Us</h1>
-            <p className='pb-4 pt-3 indent-2.5 mx-auto px-5 border rounded-2xl w-1/2 italic'>Welcome to Tazim Food Corner — where authentic Bangladeshi flavors meet the speed of fast food.
+            <p className='pb-4 pt-3 indent-2.5 mx-auto px-5 border rounded-2xl w-1/2 italic bg-amber-100 text-black shadow-2xl'>Welcome to Tazim Food Corner — where authentic Bangladeshi flavors meet the speed of fast food.
                 We believe great food shouldn't make you wait, and comfort food should never compromise on taste.
                 From crispy singara to juicy chicken fry,
                 from classic dal-bhaat to spicy fuchka — every item on our menu is crafted with fresh ingredients and the warmth of homestyle cooking. 
